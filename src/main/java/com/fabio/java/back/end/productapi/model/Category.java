e9727a7e8d6c1c14025ecb6f352a2ec28c3f2c98
FabioJRMoreira <tulkas.fabio@hotmail.com>
2022-06-05 20:25:38 -0400

    commit entity e dto

---
+++ src/main/java/com/fabio/java/back/end/productapi/model/Category.java
@@ -0,0 +1,32 @@
+package com.fabio.java.back.end.productapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fabio.java.back.end.productapi.dto.CategoryDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "category")
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String nome;
+
+	public static Category convert(CategoryDTO categoryDTO) {
+		Category category = new Category();
+		category.setId(categoryDTO.getId());
+		category.setNome(categoryDTO.getNome());
+		return category;
+	}
+}
